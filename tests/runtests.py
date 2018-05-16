a3c15b226e13a03f04a4897d28f651e406c2ece9
elapouya <elapouya@gmail.com>
2018-05-16 14:03:37 +0200

    Fix runtests.py

--- tests/runtests.py
+++ tests/runtests.py
@@ -6,7 +6,7 @@ tests = sorted(glob.glob('[A-Za-z]*.py'))
 excludes = ['runtests.py']
 
 for test in tests:
-    if test not in excludes and test.startswith('unescape'):
+    if test not in excludes:
         six.print_('%s ...' % test)
         subprocess.call(['python','./%s' % test])
 
