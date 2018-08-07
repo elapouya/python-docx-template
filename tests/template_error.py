7f994dc107a3b4b1f00fe82e90ca8ed6549f8d00
elapouya <elapouya@gmail.com>
2018-08-07 10:51:19 +0200

    Use 'six' in template_error.py test

--- tests/template_error.py
+++ tests/template_error.py
@@ -1,15 +1,22 @@
 from docxtpl import DocxTemplate, RichText
 from jinja2.exceptions import TemplateError
+import six
 
+six.print_('=' * 80)
+six.print_("Generating template error for testing (so it is safe to ignore) :")
+six.print_('.' * 80)
 try:
     tpl = DocxTemplate('test_files/template_error_tpl.docx')
     tpl.render({
         'test_variable' : 'test variable value'
     })
 except TemplateError as the_error:
-    print unicode(the_error)
+    six.print_(six.text_type(the_error))
     if hasattr(the_error, 'docx_context'):
         print "Context:"
         for line in the_error.docx_context:
-            print line
+            six.print_(line)
 tpl.save('test_files/template_error.docx')
+six.print_('.' * 80)
+six.print_(" End of TemplateError Test ")
+six.print_('=' * 80)
