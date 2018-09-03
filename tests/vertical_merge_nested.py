aa903ac99f659dce082dbe2650f46e3fa11450ff
elapouya <elapouya@gmail.com>
2018-09-03 10:35:20 +0200

    Testing new cell vertical merge

--- tests/vertical_merge_nested.py
+++ tests/vertical_merge_nested.py
@@ -1,5 +1,5 @@
 from docxtpl import DocxTemplate
 
-tpl = DocxTemplate('test_files/vertical_merge_nested.docx')
-tpl.render()
-tpl.save('test_files/word2016.docx')
+tpl = DocxTemplate('test_files/vertical_merge_nested_tpl.docx')
+tpl.render({})
+tpl.save('test_files/vertical_merge_nested.docx')
