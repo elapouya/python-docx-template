7e72603441999aa64aa2555d25219fcd3a735267
Max P <48almonds@gmail.com>
2018-11-05 18:29:33 -0800

    Added test files for the auto escape.

--- tests/escape_auto.py
+++ tests/escape_auto.py
@@ -1,12 +1,13 @@
 from docxtpl import *
 
-tpl = DocxTemplate("test_files/escape_tpl.docx")
+tpl = DocxTemplate("test_files/escape_tpl_auto.docx")
 
 context = {'myvar': R('"less than" must be escaped : <, this can be done with RichText() or R()'),
            'myescvar':'It can be escaped with a "|e" jinja filter in the template too : < ',
            'nlnp' : R('Here is a multiple\nlines\nstring\aand some\aother\aparagraphs\aNOTE: the current character styling is removed'),
            'mylisting': Listing('the listing\nwith\nsome\nlines\nand special chars : <>&'),
+           'autoescape': """These string should be auto escaped for an XML Word document which may contain <, >, &, ", and '."""
            }
 
 tpl.render(context)
-tpl.save("test_files/escape.docx")
+tpl.save("test_files/escape_auto.docx")
