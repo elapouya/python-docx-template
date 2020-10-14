cf8e493e93e4584c234ddddbc450bf8c1e8870d1
Eric Lapouyade <elapouya@gmail.com>
2020-10-14 16:52:58 +0200

    Update escape.py to test new feature

--- tests/escape.py
+++ tests/escape.py
@@ -14,6 +14,9 @@ context = {
         'the listing\nwith\nsome\nlines\nand special chars : <>&\f ... and a page break'
     ),
     'page_break': R('\f'),
+    'new_listing': """With the latest version of docxtpl,
+there is no need to use Listing objects anymore.
+Just use \\n for newline,\n\\t for \ttabulation and \\f for ...\f...page break"""
 }
 
 tpl.render(context)
