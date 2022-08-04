650b28d8526b2d08b932ab234a2c6fe0215b9009
Eric Lapouyade <elapouya@gmail.com>
2022-08-04 10:31:45 +0200

    flake8 issue

--- tests/richtext_eastAsia.py
+++ tests/richtext_eastAsia.py
@@ -6,7 +6,7 @@ Created : 2022-08-03
 
 
 from docxtpl import DocxTemplate, RichText
-tpl=DocxTemplate('templates/richtext_eastAsia_tpl.docx')
+tpl = DocxTemplate('templates/richtext_eastAsia_tpl.docx')
 rt = RichText('测试TEST', font='eastAsia:Microsoft YaHei')
 ch = RichText('测试TEST', font='eastAsia:微软雅黑')
 sun = RichText('测试TEST', font='eastAsia:SimSun')
