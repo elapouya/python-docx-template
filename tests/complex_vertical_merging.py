a282973ea3fb52b8fff8112dfd202b848d1edbed
tsy <996929281@qq.com>
2022-11-04 13:29:35 +0800

    Add files via upload

---
+++ tests/complex_vertical_merging.py
@@ -0,0 +1,12 @@
+from docxtpl import DocxTemplate
+from docxtpldvm import DVM
+
+tpl=DocxTemplate('templates/complex_vertical_merging_tpl.docx')
+context = {
+  't':range(1,10),
+  'B':DVM([(1,3),(4,6),(7,9)]),
+  'C':DVM([(3,6),(8,9)],["BBBBBB","EEEEEEE","FF0000","00FF00","0000FF"]),
+  'D':DVM([(1,2),(3,7)],["FF0000","0000FF"]),
+}
+tpl.render(context)
+tpl.save('output/complex_vertical_merging.docx')
