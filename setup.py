b48ff482c7aaf01ef700e151ab8ee3d9115fa1b9
Waket Zheng <waketzheng@gmail.com>
2025-07-16 15:49:32 +0800

    feat: use src layout

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ setup(
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     keywords="jinja2",
     url="https://github.com/elapouya/python-docx-template",
